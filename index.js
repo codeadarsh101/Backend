8f78cdc96bd3a1c879ab68ae42556e3b6e25d9e4
coseadarsh101 <adarshagrahari718@gmail.com>
2024-01-11 17:33:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+
+const express=require('express');
+
+const app=express();
+const port=3000;
+
+app.get('/',(req,res)=>{
+    res.send("here is Home Page");
+})
+
+app.get('/login',(req,res)=>{
+     res.send('<h2>Login Page</h2>')
+})
+
+app.get('/about',(req,res)=>{
+   res.send('About Section')
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App is Listening on port ${port}`);
+})
